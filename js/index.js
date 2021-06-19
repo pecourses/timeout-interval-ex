bce442d0ead521c69da6d16683dd0b9503c148f2
Viktoriia <pecourses@gmail.com>
2021-06-19 17:59:26 +0300

    timer example

--- js/index.js
+++ js/index.js
@@ -28,3 +28,47 @@
 // callback выполнится с периодичностью как минимум delay ms
 // intervalId = setInterval(callback, delay);
 // clearInterval(intervalId);
+
+const timerDiv = document.getElementById('timer');
+const [startBtn, stopBtn, resetBtn] = document.querySelectorAll('button');
+
+const DELAY = 10;
+let intervalId = null;
+let time = new Date();
+time.setMinutes(0, 0, 0);
+
+startBtn.addEventListener('click', () => {
+  if (intervalId) return;
+  intervalId = setInterval(addTime, DELAY);
+});
+
+stopBtn.addEventListener('click', () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+});
+
+resetBtn.addEventListener('click', () => {
+  time.setMinutes(0, 0, 0);
+  updateTime();
+});
+
+function addTime() {
+  time.setMilliseconds(time.getMilliseconds() + DELAY);
+  updateTime();
+}
+
+function updateTime() {
+  timerDiv.textContent = `${timeFormat2(time.getMinutes())}:${timeFormat2(
+    time.getSeconds()
+  )}.${timeFormat3(time.getMilliseconds())}`;
+}
+
+function timeFormat2(value) {
+  return value > 9 ? value : `0${value}`;
+}
+
+function timeFormat3(value) {
+  return value > 99 ? value : value > 9 ? `0${value}` : `00${value}`;
+}
