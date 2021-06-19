44735b8b024a50c750ffdab905bddf327691891b
Viktoriia <pecourses@gmail.com>
2021-06-19 17:10:30 +0300

    initial commit, add timeout interal ex

---
+++ js/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// const [btn1, btn2] = document.querySelectorAll('button');
+// let timeoutId;
+
+// btn1.addEventListener('click', e => {
+//   console.log('Hi!');
+
+//   timeoutId = setTimeout(() => {
+//     console.log('Hi after 10s!');
+//   }, 10000);
+// });
+
+// btn2.addEventListener('click', e => {
+//   clearTimeout(timeoutId);
+// });
+
+// callback выполнится 1 раз  через как минимум delay ms
+// timeoutId = setTimeout(callback, delay);
+// clearTimeout(timeoutId);
+
+// let count = 0;
+// const intervalId = setInterval(() => {
+//   console.log(count++);
+// }, 1000);
+// clearInterval(intervalId);
+
+// callback выполнится с периодичностью как минимум delay ms
+// intervalId = setInterval(callback, delay);
+// clearInterval(intervalId);
