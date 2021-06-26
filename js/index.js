7972f0df7ab0b7c2fa703986ff9e7a3b6499c92f
Viktoriia <pecourses@gmail.com>
2021-06-26 18:16:49 +0300

    initial commit

--- js/index.js
+++ js/index.js
@@ -29,46 +29,57 @@
 // intervalId = setInterval(callback, delay);
 // clearInterval(intervalId);
 
-const timerDiv = document.getElementById('timer');
-const [startBtn, stopBtn, resetBtn] = document.querySelectorAll('button');
+// const timerDiv = document.getElementById('timer');
+// const [startBtn, stopBtn, resetBtn] = document.querySelectorAll('button');
 
-const DELAY = 10;
-let intervalId = null;
-let time = new Date();
-time.setMinutes(0, 0, 0);
+// const DELAY = 10;
+// let intervalId = null;
+// let time = new Date();
+// time.setMinutes(0, 0, 0);
 
-startBtn.addEventListener('click', () => {
-  if (intervalId) return;
-  intervalId = setInterval(addTime, DELAY);
-});
+// startBtn.addEventListener('click', () => {
+//   if (intervalId) return;
+//   intervalId = setInterval(addTime, DELAY);
+// });
 
-stopBtn.addEventListener('click', () => {
-  if (intervalId) {
-    clearInterval(intervalId);
-    intervalId = null;
+// stopBtn.addEventListener('click', () => {
+//   if (intervalId) {
+//     clearInterval(intervalId);
+//     intervalId = null;
+//   }
+// });
+
+// resetBtn.addEventListener('click', () => {
+//   time.setMinutes(0, 0, 0);
+//   updateTime();
+// });
+
+// function addTime() {
+//   time.setMilliseconds(time.getMilliseconds() + DELAY);
+//   updateTime();
+// }
+
+// function updateTime() {
+//   timerDiv.textContent = `${timeFormat2(time.getMinutes())}:${timeFormat2(
+//     time.getSeconds()
+//   )}.${timeFormat3(time.getMilliseconds())}`;
+// }
+
+// function timeFormat2(value) {
+//   return value > 9 ? value : `0${value}`;
+// }
+
+// function timeFormat3(value) {
+//   return value > 99 ? value : value > 9 ? `0${value}` : `00${value}`;
+// }
+
+setTimeout(() => {
+  for (let i = 0; i < 100000; i++) {
+    console.log(i);
   }
-});
+}, 0);
 
-resetBtn.addEventListener('click', () => {
-  time.setMinutes(0, 0, 0);
-  updateTime();
-});
-
-function addTime() {
-  time.setMilliseconds(time.getMilliseconds() + DELAY);
-  updateTime();
-}
-
-function updateTime() {
-  timerDiv.textContent = `${timeFormat2(time.getMinutes())}:${timeFormat2(
-    time.getSeconds()
-  )}.${timeFormat3(time.getMilliseconds())}`;
-}
-
-function timeFormat2(value) {
-  return value > 9 ? value : `0${value}`;
-}
-
-function timeFormat3(value) {
-  return value > 99 ? value : value > 9 ? `0${value}` : `00${value}`;
-}
+console.log('AFTER FOR 1');
+console.log('AFTER FOR 2');
+console.log('AFTER FOR 3');
+console.log('AFTER FOR 4');
